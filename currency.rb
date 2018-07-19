102679aa701a3e6e196aa38ef85b977042584493
Christina Ng <christinasy317@gmail.com>
2018-07-19 18:49:28 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,13 +1,11 @@
 # Write your cod here
 
 def usd_to_eur(dollar)
-  dollar.to_i
-  euro = dollar/0.86
-  puts euro
+  euro = dollar/1.2
+  return 
 
 end
 
 def eur_to_usd(euro)
-  euro.to_i
   dollar = euro * 1.2
 end
