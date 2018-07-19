4906cbbe66b65c80aa5ff8ec6443463ff9ef0645
Christina Ng <christinasy317@gmail.com>
2018-07-19 18:53:15 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,12 +1,25 @@
 # Write your cod here
 
-def usd_to_eur(dollar)
+def usd_to_eur(dollar) #dollar worth more 
   euro = dollar/1.2
   return euro
 
 end
 
-def eur_to_usd(euro)
+def eur_to_usd(euro) 
   dollar = euro * 1.2
   return dollar
 end
+
+def usd_to_jpy(dollar)
+  yen = dollar/112.44
+  return yen  
+end
+
+def jpy_to_usd(yen)
+  dollar = yen * 0.0089
+  return dollar
+end 
+
+
+
